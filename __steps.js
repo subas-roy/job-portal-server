8efab0a48f4fe5b347611a8443c448e8898a2968
subas-roy <subasroy46@gmail.com>
2025-07-30 17:19:06 +0600

    Basic project setup

---
+++ __steps.js
@@ -0,0 +1,7 @@
+/**
+ * mkdir project-name - Create a directory
+ * cd project-name - Change into the directory
+ * npm init -y - Initialize a new npm project
+ * npm i express cors dotenv mongodb - Install necessary packages
+ * 
+ */
